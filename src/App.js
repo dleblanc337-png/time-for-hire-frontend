a3b7498c4d5c0000e1b33bfdd6e7a20bd3f06e2d
dleblanc337-png <dleblanc337@gmail.com>
2025-11-30 13:38:45 -0800

    Fix undefined isLoggedIn and handleLogout in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-// OPTIONAL: add these later if needed
-// import Contact from "./pages/Contact";
-// import Services from "./pages/Services";
-
 function App() {
   return (
     <Router>
-      <Header />   {/* ALWAYS AT TOP */}
-<Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      {/* Header no longer expects props */}
+      <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* 404 fallback */}
-        <Route path="*" element={<div style={{ padding: "20px" }}>Page not found</div>} />
       </Routes>
     </Router>
   );
