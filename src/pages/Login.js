04e0c212bc91ea13ff16748b9e1c8ef2b57f6444
dleblanc337-png <dleblanc337@gmail.com>
2025-11-30 12:29:59 -0800

    Fix missing Link import

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const API_URL = "https://time-for-hire-backend.onrender.com/api";
 
