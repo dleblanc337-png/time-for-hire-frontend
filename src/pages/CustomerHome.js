5fa3701cbb2d6e31c6704bf23b98f06184b0963e
dleblanc337-png <dleblanc337@gmail.com>
2025-12-05 10:57:24 -0800

    Add missing CustomerHome and CustomerProfile pages

---
+++ src/pages/CustomerHome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardLayout from "../components/DashboardLayout";
+
+function CustomerHome() {
+  return (
+    <DashboardLayout role="customer">
+      <h1>Home</h1>
+      <p>Customer home overview coming soon.</p>
+    </DashboardLayout>
+  );
+}
+
+export default CustomerHome;
---
+++ src/pages/CustomerProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardLayout from "../components/DashboardLayout";
+
+function CustomerProfile() {
+  return (
+    <DashboardLayout role="customer">
+      <h1>Profile</h1>
+      <p>Your personal details and settings will go here.</p>
+    </DashboardLayout>
+  );
+}
+
+export default CustomerProfile;
